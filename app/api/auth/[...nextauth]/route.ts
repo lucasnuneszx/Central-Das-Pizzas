5d2b04957436ed80e313f58292f1c9d7a73ced42
Lucas Nunes <lucasnuneszx@users.noreply.github.com>
2025-12-06 02:40:53 -0300

    refactor: remove arquivos e dependencias NextAuth

--- app/api/auth/[...nextauth]/route.ts
+++
@@ -1,9 +0,0 @@
-import NextAuth from "next-auth"
-import { authOptions } from "@/lib/auth-config"
-
-// Configurar para funcionar em qualquer host/rede
-const handler = NextAuth(authOptions)
-
-export { handler as GET, handler as POST }
-
-
